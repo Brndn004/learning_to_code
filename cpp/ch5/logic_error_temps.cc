29e5516a542266805ed5e6d2a2d784e341d61edf
Brandon Draper <bd@applied.co>
2020-04-27 23:46:09 -0700

    Finishing up ch5, then on to exercises

--- cpp/ch5/logic_error_temps.cc
+++ cpp/ch5/logic_error_temps.cc
@@ -3,17 +3,31 @@
 int main() {
   vector<double> temps;
 
+  string prompt{"Enter all temperatures (CTRL+D to quit): \n"};
+  cout << prompt;
+
   for (double temp; cin >> temp; ) temps.push_back(temp);
 
   double sum{0};
-  double hi{0};
-  double lo{0};
+  double hi{NAN};
+  double lo{NAN};
+  bool got_first_value{false};
 
   for (int tt : temps) {
-    if HERE
-    if
+    if (got_first_value) {
+      if (tt > hi) hi = tt;
+      if (tt < lo) lo = tt;
+    } else {
+      hi = tt;
+      lo = tt;
+      got_first_value = true;
+    }
     sum += tt;
   }
+
+  cout << "High temp: " << hi << "\n";
+  cout << "Low temp:  " << lo << "\n";
+  cout << "Avg temp:  " << sum / temps.size() << "\n";
 
   return 0;
 }
